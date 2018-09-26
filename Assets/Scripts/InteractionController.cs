2619a6b07343e59cf173ee5d00b5708cf93e0d89
Pavlo Kvas <kvaspasha@gmail.com>
2018-09-26 07:53:57 +0300

    add mouse interaction detection

---
+++ Assets/Scripts/InteractionController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnMouseDown()
+    {
+        Debug.Log("mouse down");
+    }
+
+    public void OnMouseUp()
+    {
+        Debug.Log("on mouse up");
+    }
+}
