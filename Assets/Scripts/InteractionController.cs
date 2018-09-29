3cdecf9b6531fb3f0bb5ed3f43029199910ed5be
Pavlo Kvas <kvaspasha@gmail.com>
2018-09-29 10:28:30 +0300

    rotate object

--- Assets/Scripts/InteractionController.cs
+++ Assets/Scripts/InteractionController.cs
@@ -7,26 +7,38 @@ public class InteractionController : MonoBehaviour
 {
     public RotationTarget RotationTarget;
     public Transform handleCenterTransform;
-    private Vector3 startMousePos;
-    
+    private Vector2 startMousePos;
+    private Vector2 curMousePos;
+
+    private bool isDraging = false;
+
     public void OnMouseDown(BaseEventData eventData)
     {
-        startMousePos = Input.mousePosition;
-        RotationTarget.transform.Rotate(transform.up, 45, Space.Self);
+        var pointerEventData = eventData as PointerEventData;
+        startMousePos = pointerEventData.pressPosition;
     }
 
     public void OnMouseUp(BaseEventData eventData)
     {
+        isDraging = false;
         var pointerEventData = eventData as PointerEventData;
         var releasePos = pointerEventData.position;
-        RotationTarget.transform.Rotate(transform.up, -45, Space.Self);
-        //compute angle between initial and release handle vectors
     }
 
     public void OnMouseDrag(BaseEventData eventData)
     {
+        isDraging = true;
         var pointerEventData = eventData as PointerEventData;
-        Debug.Log(string.Format("press pos {0}, dragged pos {1}",pointerEventData.pressPosition, pointerEventData.position));
+        curMousePos = pointerEventData.position;
+
+        var handleCenterScreen = (Vector2)Camera.main.WorldToScreenPoint(handleCenterTransform.position);
+        var curVec = curMousePos - handleCenterScreen;
+        var startVec = startMousePos - handleCenterScreen;
+        var curAngle = Vector2.SignedAngle(startVec, curVec);
+
+        RotationTarget.transform.localRotation = Quaternion.Euler(0,0, curAngle);
+
         Debug.Log("mouse drag");
+        Debug.Log(string.Format("dragged pos {0}", pointerEventData.position));
     }
 }
